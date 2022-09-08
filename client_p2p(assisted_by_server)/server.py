637c4744a2bfee52386920f31e3f33134b3833e6
Sanguk Lee <dltkddnr04@icloud.com>
2022-09-08 22:32:31 +0900

    stun서버 구현 완료

--- client_p2p(assisted_by_server)/server.py
+++ client_p2p(assisted_by_server)/server.py
@@ -9,33 +9,48 @@ server_socket.bind(('', 18650))
 server_socket.listen()
 
 client_list = []
+socket_list = []
 
 def console_print(type, message):
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print('[{}][{}] {}'.format(time, type, message))
 
+def data_process(user_nick_name, user_local_ip, user_external_ip):
+    return [user_nick_name, user_local_ip, user_external_ip]
+
 def connection(connection_socket, address):
     while True:
         try:
             receive_data = connection_socket.recv(8192)
+            receive_data = pickle.loads(receive_data)
+
+            user_nick_name = receive_data[0]
+            user_local_ip = receive_data[1]
+            user_external_ip = address[0]
+
+            processed_personal_data = data_process(user_nick_name, user_local_ip, user_external_ip)
+
+            client_list.append(processed_personal_data)
 
             if not receive_data:
                 console_print('info', 'Disconnected by {}'.format(address))
                 break
 
-            processed_receive_data = pickle.loads(receive_data)
-            console_print('receive', '{} : {}'.format(processed_receive_data[0], processed_receive_data[1]))
-            for client in client_list:
-                #if client != connection_socket:
-                client.send(receive_data)
+            console_print('receive', '{} : {}'.format(receive_data[0], receive_data[1]))
+            for client_socket in socket_list:
+                if client_socket != connection_socket:
+                    client_socket.send(pickle.dumps(processed_personal_data))
+                else:
+                    client_socket.send(pickle.dumps(client_list))
 
         except:
             #console_print('info', 'Disconnected by {}'.format(address))
             #break
             pass
 
-    if client in client_list:
-        client_list.remove(client)
+    if client_socket in socket_list:
+        socket_list.remove(client_socket)
+        client_list.remove(processed_personal_data)
 
     connection_socket.close()
 
@@ -45,8 +60,8 @@ console_print('info', 'waiting for connection')
 try:
     while True:
         connection_socket, address = server_socket.accept()
-        client_list.append(connection_socket)
-        console_print('info', 'connected by {} now {} user connected'.format(address, len(client_list)))
+        socket_list.append(connection_socket)
+        console_print('info', 'connected by {} now {} user connected'.format(address, len(socket_list)))
         threading.Thread(target=connection, args=(connection_socket, address)).start()
 
 except Exception as e:
