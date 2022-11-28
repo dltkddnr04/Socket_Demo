f249074b4be162ac81d18b9c01f6a6b8a7fe390e
Sanguk Lee <dltkddnr04@icloud.com>
2022-11-28 22:02:42 +0900

    selectors 기반으로 서버-클라이언트 다시 짬

--- client_p2p(single)/client.py
+++ client_p2p(single)/client.py
@@ -1,50 +1,29 @@
-from socket import *
-from datetime import datetime
+import socket
 import threading
-import pickle
-import time
 
-# stun_server_domain = 'stun.agaya.network'
-# stun_server_ip = gethostbyname(stun_server_domain)
-stun_server_ip = '10.0.0.116'
-
-stun_socket = socket(AF_INET, SOCK_DGRAM)
-stun_socket.bind(('', 18650))
-
-client_p2p_socket = socket(AF_INET, SOCK_DGRAM)
-client_p2p_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-client_p2p_socket.bind(('', 18650))
-
-def get_internal_ip():
-    temporary_socket = socket(AF_INET, SOCK_DGRAM)
-    temporary_socket.connect(("8.8.8.8", 80))
-    local_ip = temporary_socket.getsockname()[0]
-    temporary_socket.close()
-    return local_ip
-
-def receive_data(socket):
+def send(s):
     while True:
-        receive_data = socket.recv(1024)
-        receive_data = receive_data.decode('utf-8')
-        print(receive_data)
+        try:
+            input_data = input('>>> ')
+            s.sendall(input_data.encode())
+            if input_data == 'exit':
+                return
+        except:
+            return
 
-def send_data(socket):
+def receive(s):
     while True:
-        send_data = input('>>> ')
-        socket.send(send_data.encode('utf-8'))
-        if send_data == '/exit':
-            exit()
+        try:
+            receive_data = s.recv(8192)
+            if receive_data != b'':
+                print("Received: {}".format(receive_data.decode()))
+            else:
+                return
+        except:
+            return
 
-stun_socket.connect((stun_server_ip, 18650))
-stun_socket.send(get_internal_ip().encode())
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 4040))
 
-opponent_user_ip = stun_socket.recv(1024)
-stun_socket.close()
-
-for i in range(5):
-    try:
-        client_p2p_socket.connect((opponent_user_ip, 18650))
-        threading.Thread(target=receive_data, args=(client_p2p_socket,)).start()
-        threading.Thread(target=send_data, args=(client_p2p_socket,)).start()
-    except:
-        continue
+threading.Thread(target=receive, args=(s,)).start()
+threading.Thread(target=send, args=(s,)).start()
---
+++ client_p2p(single)/server.py
@@ -0,0 +1,48 @@
+import socket
+import selectors
+import datetime
+
+sel = selectors.DefaultSelector()
+
+def console_print(msg):
+    print("[{}] {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), msg))
+
+def accept(sock, mask):
+    conn, addr = sock.accept() # Should be ready
+    console_print('accepted from {}'.format(addr))
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, read)
+
+def read(conn, mask):
+    data = conn.recv(1024) # Should be ready
+    peer_name = conn.getpeername()
+    if data:
+        data = data.decode()
+        console_print('[received] {}: {}'.format(peer_name, data))
+
+        if data == 'exit':
+            sel.unregister(conn)
+            conn.close()
+            console_print('closed connection to {}'.format(peer_name))
+        else:
+            # send back to all clients
+            for key, mask in sel.get_map().items():
+                if 'raddr=(' in str(mask.fileobj):
+                    mask.fileobj.sendall(data.encode())
+
+    else:
+        console_print('closing {}'.format(peer_name))
+        sel.unregister(conn)
+        conn.close()
+
+sock = socket.socket()
+sock.bind(('', 4040))
+sock.listen(100)
+sock.setblocking(False)
+sel.register(sock, selectors.EVENT_READ, accept)
+
+while True:
+    events = sel.select()
+    for key, mask in events:
+        callback = key.data
+        callback(key.fileobj, mask)
