cd16e548541d4fd1a1a8f8d9437fbd325e673a6d
Sanguk Lee <dltkddnr04@icloud.com>
2023-02-23 22:45:36 +0900

    대략적인 테스트를 위한것

--- client_p2p(single)/server.py
+++ client_p2p(single)/server.py
@@ -1,48 +1,91 @@
 import socket
 import selectors
 import datetime
+import json
 
-sel = selectors.DefaultSelector()
+SERVER_IP = ''
+SERVER_PORT = 4040
 
-def console_print(msg):
-    print("[{}] {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), msg))
+def console_print(message):
+    now_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("[{time}] {message}".format(time=now_time, message=message))
 
-def accept(sock, mask):
-    conn, addr = sock.accept() # Should be ready
+def server_run():
+    selector = selectors.DefaultSelector()
+
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock.bind((SERVER_IP, SERVER_PORT))
+    server_sock.setblocking(False)
+    server_sock.listen()
+
+    selector.register(server_sock, selectors.EVENT_READ, accept)
+    while True:
+        for (key, mask) in selector.select():
+            srv_sock, callback = key.fileobj, key.data
+            callback(srv_sock, selector)
+
+    '''
+        SelectorKey.fileobj - 소켓
+        SelectorKey.fd - 저수준 파일 디스크립터
+        SelectorKey.events - 해당 IO가 대기하는 이벤트
+        SelectorKey.data - 등록시 사용된 임의의 데이터
+    '''
+
+def accept(sock, sel):
+    conn, addr = sock.accept()
     console_print('accepted from {}'.format(addr))
     conn.setblocking(False)
     sel.register(conn, selectors.EVENT_READ, read)
 
-def read(conn, mask):
-    data = conn.recv(1024) # Should be ready
-    peer_name = conn.getpeername()
-    if data:
-        data = data.decode()
-        console_print('[received] {}: {}'.format(peer_name, data))
+def read(sock, sel):
+    # recive all data
+    payload = sock.recv(8192)
+    peer_name = sock.getpeername()
+    if payload:
+        payload = payload.decode()
+        json_data = json.loads(payload)
+        nickname = json_data["nick"]
+        message = json_data["message"]
+        client_ip = sock.getpeername()[0]
+        console_print('[received] {}: {}'.format(nickname, message))
 
-        if data == 'exit':
-            sel.unregister(conn)
-            conn.close()
+        if message == 'exit':
+            sel.unregister(sock)
+            sock.close()
             console_print('closed connection to {}'.format(peer_name))
-        else:
-            # send back to all clients
+        elif message == 'connection':
             for key, mask in sel.get_map().items():
                 if 'raddr=(' in str(mask.fileobj):
-                    mask.fileobj.sendall(data.encode())
+                    # mask.fileobj.sendall(payload.encode())
+                    # send message to all client
+                    if sock == mask.fileobj:
+                        # send all client list
+                        # get each client's ip
+                        client_list = []
+                        for key, mask in sel.get_map().items():
+                            if 'raddr=(' in str(mask.fileobj) and sock != mask.fileobj:
+                                client_ip = mask.fileobj.getpeername()[0]
+                                client_list.append(client_ip)
+                        client_list = json.dumps(client_list)
+                        sock.sendall(client_list.encode())
+
+                    else:
+                        # send new client(sock) list
+                        # get sock's ip
+                        client_list = []
+                        client_list.append(client_ip)
+                        client_list = json.dumps(client_list)
+                        mask.fileobj.sendall(client_list.encode())
+        elif message == 'ext_ip':
+            send_data = {
+                'client_ip': client_ip
+            }
+            sock.sendall(json.dumps(send_data).encode())
 
     else:
-        console_print('closing {}'.format(peer_name))
-        sel.unregister(conn)
-        conn.close()
+        console_print('closed connection to {}'.format(peer_name))
+        sel.unregister(sock)
+        sock.close()
 
-sock = socket.socket()
-sock.bind(('', 4040))
-sock.listen(100)
-sock.setblocking(False)
-sel.register(sock, selectors.EVENT_READ, accept)
-
-while True:
-    events = sel.select()
-    for key, mask in events:
-        callback = key.data
-        callback(key.fileobj, mask)
+if __name__ == '__main__':
+    server_run()
---
+++ client_p2p(single)/test_client.py
@@ -0,0 +1,60 @@
+import socket
+import datetime
+import json
+import threading
+
+SERVER_IP = ''
+SERVER_PORT = 4040
+# but client-client connection port is 4041
+
+def console_print(message):
+    now_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("[{time}] {message}".format(time=now_time, message=message))
+
+def sock_send(sock):
+    while True:
+        try:
+            message = input("message: ")
+            if message == 'exit':
+                break
+            sock.sendall(message.encode())
+        except:
+            console_print("connection error")
+            break
+
+def sock_recv(sock):
+    while True:
+        try:
+            data = sock.recv(1024)
+            if data:
+                console_print(data.decode())
+                if data.decode() == 'exit':
+                    break
+        except:
+            console_print("connection error")
+            break
+
+def server_connection():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((SERVER_IP, SERVER_PORT))
+    data = {
+        'nick': 'nick',
+        'message': 'connection'
+    }
+    sock.sendall(json.dumps(data).encode())
+
+    data = sock.recv(1024)
+    if data:
+        processed_data = json.loads(data.decode())
+        osc_ip = processed_data[0]
+
+        p2p_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        p2p_socket.connect((osc_ip, 4041))
+        while True:
+            message = input()
+            p2p_socket.sendall(message.encode())
+            threading.Thread(target=sock_recv, args=(p2p_socket,)).start()
+            threading.Thread(target=sock_send, args=(p2p_socket,)).start()
+
+if __name__ == '__main__':
+    server_connection()
